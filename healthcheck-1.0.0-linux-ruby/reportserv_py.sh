da91cc1eda8808095c4f9d31d0ae136444a50bf6
Tyler Mitchell <spatialguru@shaw.ca>
2015-12-10 16:35:54 -0800

    python http server fix

--- healthcheck-1.0.0-linux-ruby/reportserv_py.sh
+++ healthcheck-1.0.0-linux-ruby/reportserv_py.sh
@@ -1,2 +1,11 @@
-echo "Goto: http://localhost:5000/viewer/#/url/http://localhost:5000/reports/report.json"
-python -m SimpleHTTPServer 5000
+#!/bin/bash
+set -e
+
+# Figure out where this script is located.
+SELFDIR="`dirname \"$0\"`"
+APPDIR="`cd \"$SELFDIR/lib/app" && pwd`"
+BINDIR="`cd \"$SELFDIR/lib/ruby" && pwd`"
+
+cd $APPDIR
+# Run the actual app using the bundled Ruby interpreter.
+exec "$BINDIR/bin/rake" "reports:view"
