178ffbfd801dea8d01834d54039269145f71aaf9
Tyler Mitchell <spatialguru@shaw.ca>
2016-02-05 18:05:32 -0800

    net network format

---
+++ preflight-1.0.0-linux-ruby/lib/app/spec/net/eth_10GB_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "Fastest network interface speed >= 1GB" do
+	it {
+		linkspeeds = []
+		devlines=`ip -o link show`.split("\n")
+
+		devlines[1..devlines.count].each{ |d|
+			linkspeeds.push(d.split("\\")[0].split(" ")[-1])
+		}
+		expect(linkspeeds.max.to_i).to be >= 10000
+	}
+end
