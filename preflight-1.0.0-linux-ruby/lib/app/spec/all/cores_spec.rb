46c411e19b7909e5f6f33fcbc6d410f4297c5d96
Tyler Mitchell <tyler.mitchell@actian.com>
2016-02-29 14:24:21 -0600

    remove/rename some files

--- preflight-1.0.0-linux-ruby/lib/app/spec/all/cores_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-#I hate calling out directly from Ruby like this
-## but the function: "command('nproc')" was returning strings I couldn't cast
-
-describe `nproc`.to_i do
-	it { should >= 12 }
-end
-
--- preflight-1.0.0-linux-ruby/lib/app/spec/all/ulimit_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe command('ulimit') do
-        its(:value) { should be >= 65000 }
-end
